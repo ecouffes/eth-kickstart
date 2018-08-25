cceddeb8467ffeb445b251aaf43a71836fc270d1
Katsunori Tanaka <tanaka-katsunori@dmm.com>
2018-08-26 02:04:29 +0900

    Add semantic-ui-react

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,48 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import { Card, Button } from 'semantic-ui-react'
 import factory from '../ethereum/factory';
 
 class ProjectIndex extends Component {
 
-  render() {
-    return <p>Project Index!</p>
-  }
+    // Class method :initial rendering not to SSR
+    static async getInitialProps() {
+        // array
+        const projects = await factory.methods.getDeployedCrowdfunding().call();
+        // console.log(projects);
+        return { projects }
+    }
 
-  async componentDidMount() {
-    const projects = await factory.methods.getDeployedCrowdfunding().call();
-    console.log(projects);
-  }
+    renderProjects() {
+        const items = this.props.projects.map(address => {
+            return {
+                header: address,
+                description: <a href="#">View Project</a>,
+                fluid: true
+            }
+        });
+
+        return <Card.Group items={items}/>;
+    }
+
+    // cannot use async keyword
+    render() {
+        return (
+            <div>
+                <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css"></link>
+                <h2>Open Projects</h2>
+                {this.renderProjects()}
+                <Button
+                    content={"Create Project"}
+                    icon={"add"}
+                    primary
+                />
+            </div>
+        )
+    }
+
+    async componentDidMount() {
+
+    }
 }
 
 export default ProjectIndex;
