9ff1c7f92526b76190c911c0763c649c29d7213e
ecouffes <tanakakatsunori2@gmail.com>
2018-08-27 02:13:45 +0900

    Fix routing

--- routes.js
+++ routes.js
@@ -4,6 +4,8 @@ const routes = require('next-routes')();
 
 routes
     .add('/projects/new', '/projects/new')
-    .add('/projects/:address', '/projects/show');
+    .add('/projects/:address', '/projects/show')
+    .add('/projects/:address/requests', '/projects/requests/index')
+    .add('/projects/:address/requests/new', '/projects/requests/new');
 
 module.exports = routes;
