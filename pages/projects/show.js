f92bb8eb23fe2587eb2bb08582c5f791a423a082
ecouffes <tanakakatsunori2@gmail.com>
2018-08-26 22:14:38 +0900

    Fix show.js

--- pages/projects/show.js
+++ pages/projects/show.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import crowdfunding from "../../ethereum/crowdfunding";
+import web3 from "../../ethereum/web3";
 
 class ProjectShow extends Component {
 
@@ -40,7 +41,31 @@ class ProjectShow extends Component {
                 description:
                     'The manager created this project and can create requests to withdraw money',
                 style: { overflowWrap: 'break-word' }
-            }
+            },
+            {
+                header: miniumContribution,
+                meta: 'Minium Contribution (wei)',
+                description:
+                    'You must contribute at least this much wei to become a approver'
+            },
+            {
+                header: requestsCount,
+                meta: 'Number of Requests',
+                description:
+                    'A request tries to withdraw money from the contract. Request must be approved by approvers'
+            },
+            {
+                header: approversCount,
+                meta: 'Number of Approvers',
+                description:
+                    'Number of people who have already donated to this project'
+            },
+            {
+                header: web3.utils.fromWei(balance, 'ether'),
+                meta: 'Project Balance (ether)',
+                description:
+                    'The balance is how much money this project has left to spend.'
+            },
         ];
 
         return <Card.Group items={items} />
