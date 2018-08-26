35b857b4798f2480a5d6099db1cc31b60e275fc0
ecouffes <tanakakatsunori2@gmail.com>
2018-08-27 02:12:59 +0900

    Fix projects/show

--- pages/projects/show.js
+++ pages/projects/show.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Grid, Button } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
-import crowdfunding from "../../ethereum/crowdfunding";
-import web3 from "../../ethereum/web3";
+import crowdfunding from '../../ethereum/crowdfunding';
+import web3 from '../../ethereum/web3';
+import ContributeForm from '../../components/ContributeForm';
+import { Link } from '../../routes'
 
 class ProjectShow extends Component {
 
@@ -10,6 +12,7 @@ class ProjectShow extends Component {
     static async getInitialProps(props) {
         // get rooting query address
         // console.log(props.query.address)
+        // ルーティングのクエリから、コントラクトアドレスを取得
         const crowdfundingInstance = crowdfunding(props.query.address);
         const summary = await crowdfundingInstance.methods.getSummary().call();
 
@@ -17,6 +20,8 @@ class ProjectShow extends Component {
         // console.log(summary);
 
         return {
+            // コントラクトアドレスをpropsに詰める
+            address: props.query.address,
             miniumContribution: summary[0],
             balance: summary[1],
             requestsCount: summary[2],
@@ -40,7 +45,7 @@ class ProjectShow extends Component {
                 meta: 'Address of Manager',
                 description:
                     'The manager created this project and can create requests to withdraw money',
-                style: { overflowWrap: 'break-word' }
+                style: {overflowWrap: 'break-word'}
             },
             {
                 header: miniumContribution,
@@ -68,14 +73,31 @@ class ProjectShow extends Component {
             },
         ];
 
-        return <Card.Group items={items} />
+        return <Card.Group items={items}/>
     }
 
     render() {
         return (
             <Layout>
                 <h2>Project Show</h2>
-                {this.renderCard()}
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column width={10}>
+                            {this.renderCard()}
+                        </Grid.Column>
+                        <Grid.Column width={6}>
+                            {/* 子コンポーネントにコントラクトアドレスを渡す */}
+                            <ContributeForm address={this.props.address}/>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Link route={`/projects/${this.props.address}/requests`}>
+                                <a><Button primary>View Requests</Button></a>
+                            </Link>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
             </Layout>
         )
     }
