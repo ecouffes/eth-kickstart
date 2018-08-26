e06a6f557ef58a34570e278aaca25fb18d543b1f
Katsunori Tanaka <tanaka-katsunori@dmm.com>
2018-08-26 12:58:12 +0900

    Add Layout file

---
+++ components/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+import Header from './Header';
+
+export default props => {
+    return (
+        <Container>
+            <Header />
+            {props.children}
+        </Container>
+    )
+};
+
