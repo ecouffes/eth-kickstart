ea25e640a9ca8de04b52a2e6e3dedc4c8c1717d0
Katsunori Tanaka <tanaka-katsunori@dmm.com>
2018-08-26 13:26:04 +0900

    Add new.js

---
+++ pages/projects/new.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Layout from '../../components/Layout';
+
+class ProjectNew extends Component {
+    render() {
+        return (
+            <Layout>
+                <h1>hoge</h1>
+            </Layout>
+        )
+    }
+}
+
+export default ProjectNew;
--- pages/show.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default () => {
-    return <p>This is the show page!</p>;
-}
